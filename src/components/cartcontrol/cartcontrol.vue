bf93c0350c732611dd971e7ae3adceb2b33bbed9
mcx <349919340@qq.com>
2019-07-09 18:18:37 +0800

    my vue

---
+++ src/components/cartcontrol/cartcontrol.vue
@@ -0,0 +1,79 @@
+<template>
+	<div class="cartcontrol">
+		<transition name="move">
+			<div class="cart-decrease icon-remove_circle_outline" v-show="food.count>0" @click.stop.prevent="decreaseCart"></div>
+		</transition>
+		<div class="cart-count" v-show="food.count>0">{{food.count}}</div>
+		<div class="cart-add icon-add_circle" @click.stop.prevent="addCart"></div>
+	</div>
+</template>
+
+<script>
+	import Vue from 'vue'
+	export default{
+		
+		props: {
+			food: {
+				type: Object
+			}
+		},
+		created() {
+			
+		},
+		methods: {
+			addCart(event) {
+				if(!event._constructed){
+					return
+				}
+				if(!this.food.count){
+					Vue.set(this.food,'count',1)
+				}else {
+					this.food.count++
+				}
+				this.$emit('cart-add', event.target)
+			},
+			decreaseCart(event){
+				if(!event._constructed){
+					return
+				}
+				if (this.food.count){
+					this.food.count--
+				}
+			}
+		}
+	}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+	.cartcontrol
+		font-size: 0
+		.cart-decrease
+			display:inline-block
+			padding: 6px
+			display: inline-block
+			line-height: 24px
+			font-size:24px
+			color: rgb(0,160,220)
+			opacity: 1
+			transform: translate3d(0,0,0) rotate(0)
+			&.move-enter-active, &.move-leave-active
+				transition: all 0.4s linear
+			&.move-enter, &.move-leave-to
+				opacity 0
+				transform: translate3d(24px,0,0) rotate(180deg)
+		.cart-count
+			display:inline-block
+			vertical-align: top
+			width: 12px
+			padding-top: 6px
+			line-height: 24px
+			text-align:center
+			font-size: 10px
+			color: rgb(147,153,159)
+		.cart-add
+			display:inline-block
+			padding: 6px
+			line-height: 24px
+			font-size:24px
+			color: rgb(0,160,220)
+</style>
